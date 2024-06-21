93fb8c8b9810f9b9b4fccf1f02ed17b4083fd6e8
Vvyache <133092047+Vvyache@users.noreply.github.com>
2024-06-21 18:04:57 +0500

    Дипломный проект: реальный кейс от компании Силант

--- Silant/back/App/Account/adapter.py
+++
@@ -1,7 +0,0 @@
-from allauth.account.adapter import DefaultAccountAdapter
-
-
-class CustomAdapter(DefaultAccountAdapter):
-    
-    def is_open_for_signup(self, request):
-        return False
--- Silant/back/App/Handbook/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class HandbookConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Handbook'
-    verbose_name = 'Справочники'
